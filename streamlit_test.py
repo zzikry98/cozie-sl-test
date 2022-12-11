375051723484ae166df29d64c29b0b0c48f94b45
M Zikry <78649030+zzikry98@users.noreply.github.com>
2022-12-11 22:25:15 +0800

    Changed to pickle code for faster extraction

--- streamlit_test.py
+++ streamlit_test.py
@@ -21,127 +21,121 @@ print("Hello World")
 st.set_page_config(layout="wide")
 st.title("Test title")
 
-##id_participant = "orenth45"
-##id_experiment = "orenth"
-##participant_list = ["orenth01",
-##                    "orenth02",
-##                    "orenth03",
-##                    "orenth04",
-##                    "orenth05",
-##                    "orenth06",
-##                    "orenth07",
-##                    "orenth08",
-##                    "orenth09",
-##                    "orenth10",
-##                    "orenth11",
-##                    "orenth12",
-##                    "orenth13",
-##                    "orenth14",
-##                    "orenth15",
-##                    "orenth16",
-##                    "orenth17",
-##                    "orenth18",
-##                    "orenth19",
-##                    "orenth20",
-##                    "orenth21",
-##                    "orenth22",
-##                    "orenth23",
-##                    "orenth24",
-##                    "orenth25",
-##                    "orenth26",
-##                    "orenth27",
-##                    "orenth28",
-##                    "orenth29",
-##                    "orenth30",
-##                    "orenth31",
-##                    "orenth32",
-##                    "orenth33",
-##                    "orenth34",
-##                    "orenth35",
-##                    "orenth36",
-##                    "orenth37",
-##                    "orenth38",
-##                    "orenth39",
-##                    "orenth40",
-##                    "orenth41",
-##                    "orenth40",
-##                    "orenth41",
-##                    "orenth42",
-##                    "orenth43",
-##                    "orenth44"]
-##YOUR_TIMEZONE = "Asia/Singapore"
-##
 start_of_extraction = time.time()
-### Initialize InfluxDB client
-##host = "lonepine-64d016d6.influxcloud.net"
-##port = 8086
-##user = "Cozie-Apple-Freelancer"
-##password = "D4YuzwYsxVWmjHZ4Q6J0"
-##database = "cozie-apple"
-##client = DataFrameClient(host, port, user, password, database, ssl=True, verify_ssl=True)
-##
-### Query influx
-##query = f'SELECT * FROM "cozie-apple"."autogen"."{id_experiment}" WHERE "time">now()-10w'
-##
-##print("Query:", query)
-##
-### Get result
-##result = client.query(query)
-###print("\nResult:\n", result)
-##df = pd.DataFrame.from_dict(result[id_experiment])
-##df = df.sort_index(ascending=False)
-##
-### Convert index to local timezone
-##df.index = df.index.tz_convert(YOUR_TIMEZONE)
-##
-### Order timestmaps
-##df.sort_index(inplace=True, ascending=True)
-##
-### Parse timestamp_lambda
-##df["timestamp_lambda"] = pd.to_datetime(df["timestamp_lambda"], format="%Y-%m-%dT%H:%M:%S.%fZ", errors='coerce')
+
+id_participant = "orenth45"
+id_experiment = "orenth"
+participant_list = ["orenth01",
+                    "orenth02",
+                    "orenth03",
+                    "orenth04",
+                    "orenth05",
+                    "orenth06",
+                    "orenth07",
+                    "orenth08",
+                    "orenth09",
+                    "orenth10",
+                    "orenth11",
+                    "orenth12",
+                    "orenth13",
+                    "orenth14",
+                    "orenth15",
+                    "orenth16",
+                    "orenth17",
+                    "orenth18",
+                    "orenth19",
+                    "orenth20",
+                    "orenth21",
+                    "orenth22",
+                    "orenth23",
+                    "orenth24",
+                    "orenth25",
+                    "orenth26",
+                    "orenth27",
+                    "orenth28",
+                    "orenth29",
+                    "orenth30",
+                    "orenth31",
+                    "orenth32",
+                    "orenth33",
+                    "orenth34",
+                    "orenth35",
+                    "orenth36",
+                    "orenth37",
+                    "orenth38",
+                    "orenth39",
+                    "orenth40",
+                    "orenth41",
+                    "orenth40",
+                    "orenth41",
+                    "orenth42",
+                    "orenth43",
+                    "orenth44"]
+YOUR_TIMEZONE = "Asia/Singapore"
+
+
+### --- New extraction code ---
+s3_pickle_path = "https://cozie-dashboard.s3.ap-southeast-1.amazonaws.com/cozie_orenth_pickle.zip"
+response = requests.get(s3_pickle_path)
+
+open("s3_pickle.zip", "wb").write(response.content)
+df = pd.read_pickle("./s3_pickle.zip")
+### --- End of new extraction code ---
+
+df = pd.DataFrame.from_dict(df)
+df = df.sort_index(ascending=False)
+
+# Convert index to local timezone
+df.index = df.index.tz_convert(YOUR_TIMEZONE)
+
+# Order timestmaps
+df.sort_index(inplace=True, ascending=True)
+
+# Parse timestamp_lambda
+df["timestamp_lambda"] = pd.to_datetime(df["timestamp_lambda"], format="%Y-%m-%dT%H:%M:%S.%fZ", errors='coerce')
 ##df["timestamp_lambda"] = df["timestamp_lambda"].dt.tz_localize("UTC")
-##df["timestamp_lambda"] = df["timestamp_lambda"].dt.tz_convert("Asia/Singapore")
-##
-### Parse timestamp_location
-##df["timestamp_location"] = pd.to_datetime(df["timestamp_location"], format="%Y-%m-%dT%H:%M:%S.%fZ", errors='coerce')
+df["timestamp_lambda"] = df["timestamp_lambda"].dt.tz_convert("Asia/Singapore")
+
+# Parse timestamp_location
+df["timestamp_location"] = pd.to_datetime(df["timestamp_location"], format="%Y-%m-%dT%H:%M:%S.%fZ", errors='coerce')
 ##df["timestamp_location"] = df["timestamp_location"].dt.tz_localize("UTC")
-##df["timestamp_location"] = df["timestamp_location"].dt.tz_convert("Asia/Singapore")
-##
-### Parse timestamp_start
-##df["timestamp_start"] = pd.to_datetime(df["timestamp_start"], format="%Y-%m-%dT%H:%M:%S.%fZ", errors='coerce')
+df["timestamp_location"] = df["timestamp_location"].dt.tz_convert("Asia/Singapore")
+
+# Parse timestamp_start
+df["timestamp_start"] = pd.to_datetime(df["timestamp_start"], format="%Y-%m-%dT%H:%M:%S.%fZ", errors='coerce')
 ##df["timestamp_start"] = df["timestamp_start"].dt.tz_localize("UTC")
-##df["timestamp_start"] = df["timestamp_start"].dt.tz_convert("Asia/Singapore")
-##
-### Compute timestamp differences
-##df['timestamp'] = df.index
-##grouped = df[df["vote_count"].notna()].groupby(['id_participant'])
-##
-##df['dT'] = (grouped['timestamp'].diff()
-##.astype('timedelta64[ms]')
-##.div(1000)
-##.div(60))
-##
-### Remove 'orenth' and 'admin' from 'id_participant
-##df = df[~((df["id_participant"]=='admin') | (df["id_participant"]=='orenth'))]
-##
-### Create backup of df
-##df_storage = df.copy()
-##df.head()
-##
-### Get fresh copy of dataframe
-##df = df_storage.copy()
-###df = df[(df.index>date_start) | (df.id_participant==id_participant)]
-##
-### Compute Counts
-##df = df[df["vote_count"].notna()].groupby("id_participant")["vote_count"].count()
-##
-##df.to_csv("df.csv")
+df["timestamp_start"] = df["timestamp_start"].dt.tz_convert("Asia/Singapore")
 
-df = pd.read_csv("df.csv")["vote_count"]
+# Compute timestamp differences
+df['timestamp'] = df.index
+grouped = df[df["vote_count"].notna()].groupby(['id_participant'])
 
+df['dT'] = (grouped['timestamp'].diff()
+.astype('timedelta64[ms]')
+.div(1000)
+.div(60))
+
+# Remove 'orenth' and 'admin' from 'id_participant
+df = df[~((df["id_participant"]=='admin') | (df["id_participant"]=='orenth'))]
+
+# Create backup of df
+df_storage = df.copy()
+df.head()
+
+# Get fresh copy of dataframe
+df = df_storage.copy()
+#df = df[(df.index>date_start) | (df.id_participant==id_participant)]
+
+# Compute Counts
+df = df[df["vote_count"].notna()].groupby("id_participant")["vote_count"].count()
+
+df.to_csv("df.csv")
 # --- END OF EXTRACTING DATA ---
 stop_of_extraction = time.time()
 print(f"Time taken to extract and format data: {stop_of_extraction - start_of_extraction} seconds")
+print("Hello World Done")
+
+df.to_csv("df.csv")
 
 # --- START OF BAR PLOTS ---
 start_of_plotting_bar_1 = time.time()
