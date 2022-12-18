28606a475b0f5bf328e1f0b8b0800ea2c31f0766
M Zikry <78649030+zzikry98@users.noreply.github.com>
2022-12-18 18:31:55 +0800

    Changed plots to plotly

--- streamlit_test.py
+++ streamlit_test.py
@@ -4,206 +4,193 @@ import pandas as pd
 import numpy as np
 import time
 import datetime
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates  
-from matplotlib.ticker import FormatStrFormatter
-import matplotlib.patches as mpatches
-import seaborn as sns
 import pytz
 from pytz import timezone
 import math
 from textwrap import wrap
 from influxdb import DataFrameClient
 import streamlit as st
+import plotly.express as px
+import plotly          
+import plotly.graph_objects as go
 
 print("Hello World")
 
-st.set_page_config(layout="wide")
-st.title("Test title (Pickle test)")
+st.set_page_config(layout="wide", page_icon=":watch:")
+st.title("Cozie dashboard")
 
-start_of_extraction = time.time()
+with st.spinner("Loading data..."):
+  start_of_extraction = time.time()
 
-id_participant = "orenth45"
-id_experiment = "orenth"
-participant_list = ["orenth01",
-                    "orenth02",
-                    "orenth03",
-                    "orenth04",
-                    "orenth05",
-                    "orenth06",
-                    "orenth07",
-                    "orenth08",
-                    "orenth09",
-                    "orenth10",
-                    "orenth11",
-                    "orenth12",
-                    "orenth13",
-                    "orenth14",
-                    "orenth15",
-                    "orenth16",
-                    "orenth17",
-                    "orenth18",
-                    "orenth19",
-                    "orenth20",
-                    "orenth21",
-                    "orenth22",
-                    "orenth23",
-                    "orenth24",
-                    "orenth25",
-                    "orenth26",
-                    "orenth27",
-                    "orenth28",
-                    "orenth29",
-                    "orenth30",
-                    "orenth31",
-                    "orenth32",
-                    "orenth33",
-                    "orenth34",
-                    "orenth35",
-                    "orenth36",
-                    "orenth37",
-                    "orenth38",
-                    "orenth39",
-                    "orenth40",
-                    "orenth41",
-                    "orenth40",
-                    "orenth41",
-                    "orenth42",
-                    "orenth43",
-                    "orenth44"]
-YOUR_TIMEZONE = "Asia/Singapore"
+  id_participant = "orenth45"
+  id_experiment = "orenth"
+  participant_list = ["orenth01",
+                      "orenth02",
+                      "orenth03",
+                      "orenth04",
+                      "orenth05",
+                      "orenth06",
+                      "orenth07",
+                      "orenth08",
+                      "orenth09",
+                      "orenth10",
+                      "orenth11",
+                      "orenth12",
+                      "orenth13",
+                      "orenth14",
+                      "orenth15",
+                      "orenth16",
+                      "orenth17",
+                      "orenth18",
+                      "orenth19",
+                      "orenth20",
+                      "orenth21",
+                      "orenth22",
+                      "orenth23",
+                      "orenth24",
+                      "orenth25",
+                      "orenth26",
+                      "orenth27",
+                      "orenth28",
+                      "orenth29",
+                      "orenth30",
+                      "orenth31",
+                      "orenth32",
+                      "orenth33",
+                      "orenth34",
+                      "orenth35",
+                      "orenth36",
+                      "orenth37",
+                      "orenth38",
+                      "orenth39",
+                      "orenth40",
+                      "orenth41",
+                      "orenth40",
+                      "orenth41",
+                      "orenth42",
+                      "orenth43",
+                      "orenth44"]
+  YOUR_TIMEZONE = "Asia/Singapore"
 
 
-### --- New extraction code ---
-s3_pickle_path = "https://cozie-dashboard.s3.ap-southeast-1.amazonaws.com/cozie_orenth_pickle.zip"
-response = requests.get(s3_pickle_path)
+  ### --- New extraction code ---
+  s3_pickle_path = "https://cozie-dashboard.s3.ap-southeast-1.amazonaws.com/cozie_orenth_pickle.zip"
+  response = requests.get(s3_pickle_path)
 
-open("s3_pickle.zip", "wb").write(response.content)
-df = pd.read_pickle("./s3_pickle.zip")
-### --- End of new extraction code ---
+  open("s3_pickle.zip", "wb").write(response.content)
+  df = pd.read_pickle("./s3_pickle.zip")
+  ### --- End of new extraction code ---
 
-df = pd.DataFrame.from_dict(df)
-df = df.sort_index(ascending=False)
+  df = pd.DataFrame.from_dict(df)
+  df = df.sort_index(ascending=False)
 
-# Convert index to local timezone
-df.index = df.index.tz_convert(YOUR_TIMEZONE)
+  # Convert index to local timezone
+  df.index = df.index.tz_convert(YOUR_TIMEZONE)
 
-# Order timestmaps
-df.sort_index(inplace=True, ascending=True)
+  # Order timestmaps
+  df.sort_index(inplace=True, ascending=True)
 
-# Parse timestamp_lambda
-df["timestamp_lambda"] = pd.to_datetime(df["timestamp_lambda"], format="%Y-%m-%dT%H:%M:%S.%fZ", errors='coerce')
-##df["timestamp_lambda"] = df["timestamp_lambda"].dt.tz_localize("UTC")
-df["timestamp_lambda"] = df["timestamp_lambda"].dt.tz_convert("Asia/Singapore")
+  # Parse timestamp_lambda
+  df["timestamp_lambda"] = pd.to_datetime(df["timestamp_lambda"], format="%Y-%m-%dT%H:%M:%S.%fZ", errors='coerce')
+  ##df["timestamp_lambda"] = df["timestamp_lambda"].dt.tz_localize("UTC")
+  df["timestamp_lambda"] = df["timestamp_lambda"].dt.tz_convert("Asia/Singapore")
 
-# Parse timestamp_location
-df["timestamp_location"] = pd.to_datetime(df["timestamp_location"], format="%Y-%m-%dT%H:%M:%S.%fZ", errors='coerce')
-##df["timestamp_location"] = df["timestamp_location"].dt.tz_localize("UTC")
-df["timestamp_location"] = df["timestamp_location"].dt.tz_convert("Asia/Singapore")
+  # Parse timestamp_location
+  df["timestamp_location"] = pd.to_datetime(df["timestamp_location"], format="%Y-%m-%dT%H:%M:%S.%fZ", errors='coerce')
+  ##df["timestamp_location"] = df["timestamp_location"].dt.tz_localize("UTC")
+  df["timestamp_location"] = df["timestamp_location"].dt.tz_convert("Asia/Singapore")
 
-# Parse timestamp_start
-df["timestamp_start"] = pd.to_datetime(df["timestamp_start"], format="%Y-%m-%dT%H:%M:%S.%fZ", errors='coerce')
-##df["timestamp_start"] = df["timestamp_start"].dt.tz_localize("UTC")
-df["timestamp_start"] = df["timestamp_start"].dt.tz_convert("Asia/Singapore")
+  # Parse timestamp_start
+  df["timestamp_start"] = pd.to_datetime(df["timestamp_start"], format="%Y-%m-%dT%H:%M:%S.%fZ", errors='coerce')
+  ##df["timestamp_start"] = df["timestamp_start"].dt.tz_localize("UTC")
+  df["timestamp_start"] = df["timestamp_start"].dt.tz_convert("Asia/Singapore")
 
-# Compute timestamp differences
-df['timestamp'] = df.index
-grouped = df[df["vote_count"].notna()].groupby(['id_participant'])
+  # Compute timestamp differences
+  df['timestamp'] = df.index
+  grouped = df[df["vote_count"].notna()].groupby(['id_participant'])
 
-df['dT'] = (grouped['timestamp'].diff()
-.astype('timedelta64[ms]')
-.div(1000)
-.div(60))
+  df['dT'] = (grouped['timestamp'].diff()
+  .astype('timedelta64[ms]')
+  .div(1000)
+  .div(60))
 
-# Remove 'orenth' and 'admin' from 'id_participant
-df = df[~((df["id_participant"]=='admin') | (df["id_participant"]=='orenth'))]
+  # Remove 'orenth' and 'admin' from 'id_participant
+  df = df[~((df["id_participant"]=='admin') | (df["id_participant"]=='orenth'))]
 
-# Create backup of df
-df_storage = df.copy()
-df.head()
+  # Create backup of df
+  df_storage = df.copy()
+  df.head()
 
-# Get fresh copy of dataframe
-df = df_storage.copy()
-#df = df[(df.index>date_start) | (df.id_participant==id_participant)]
+  # Get fresh copy of dataframe
+  df = df_storage.copy()
+  #df = df[(df.index>date_start) | (df.id_participant==id_participant)]
 
-# Compute Counts
-df = df[df["vote_count"].notna()].groupby("id_participant")["vote_count"].count()
+  # Compute Counts
+  df = df[df["vote_count"].notna()].groupby("id_participant")["vote_count"].count()
 
-df.to_csv("df.csv")
-# --- END OF EXTRACTING DATA ---
-stop_of_extraction = time.time()
-print(f"Time taken to extract and format data: {stop_of_extraction - start_of_extraction} seconds")
-print("Hello World Done")
+  # --- END OF EXTRACTING DATA ---
+  stop_of_extraction = time.time()
+  print(f"Time taken to extract and format data: {stop_of_extraction - start_of_extraction} seconds")
+  print("Hello World Done")
 
-df.to_csv("df.csv")
+  df.to_csv("df.csv")
 
-# --- START OF BAR PLOTS ---
-start_of_plotting_bar_1 = time.time()
+  # --- PLOTLY TEST ---
 
-### Plot for cohort
-##df = df_storage.copy()
-###date_start= datetime.datetime.now().replace(tzinfo=pytz.UTC).astimezone(pytz.timezone("Asia/Singapore"))-datetime.timedelta(weeks=2)
-###df = df[df.index>date_start]
-##
-##fig, ax = plt.subplots(1,1, figsize=(30,10))
-##df2 =(df[df["vote_count"].notna()]
-## .groupby("id_participant")
-## .resample('D')["vote_count"]
-## .count()
-## .unstack()
-## .T)
-##
-##df2.plot(kind='bar', 
-##       title='vote_count counts daily, individual', 
-##       ylabel='Counts', 
-##       xlabel='Date', 
-##       figsize=(25, 7),
-##       ax=ax)
-##
-##ax.set_xticklabels(df2.index.strftime('%d.%b').tolist()) # This line is necessary because the built in bar-plot-functionality doesn't handle datetime indexes
-##ax.tick_params(axis='x', labelrotation=45) # Rotate xlabel
-###ax.legend(loc=(1.05, 0.05))
-##ax.legend(loc=(0, 1.2), ncol=10, title="Participant IDs")
+  # --- BAR CHART ---
 
-# Plot for vote counts
+  plotly_labels = {"vote_count": "Counts", "id_participant": "Participant ID", "date_time": "Date & Time"}
 
-# Compute bar colors
-bar_color = []
-labels = []
-for row in df:
-  if row>=100:
-    bar_color.append("green")
-    labels.append("complete")
-  elif row>=50:
-    bar_color.append("yellow")
-    labels.append("JITAI phase")
-  else:
-    bar_color.append("blue")
-    labels.append("Initial phase")
+  df_plotly = df.copy()
+  df_plotly=df_plotly.reset_index()
 
-# Create custom legend
-legend_elements = [mpatches.Patch(facecolor='green', edgecolor='black', label='Complete'),
-                   mpatches.Patch(facecolor='yellow', edgecolor='black', label='JITAI Phase'),
-                   mpatches.Patch(facecolor='blue',  edgecolor='black', label='Initial Phase')]
+  df_plotly['phase'] = "Initial Phase"
+  for i in df_plotly.index:
+      if df_plotly.at[i, "vote_count"] >= 100:
+          df_plotly.at[i, "phase"] = "Complete"
+      elif df_plotly.at[i, "vote_count"] >= 50:
+          df_plotly.at[i, "phase"] = "JITAI Phase"
 
-# Plot bars
-fig, ax = plt.subplots(1,1, figsize =(20, 10))
-ax = df.plot(kind='bar', 
-       title='vote_count counts overall, individual', 
-       ylabel='Counts', 
-       xlabel='Participants', 
-       color=bar_color,
-       edgecolor='black',
-       figsize=(25, 7))
-my_legend = ax.legend(handles=legend_elements, title="Phases")
-#my_legend._legend_box.align="left"
+  fig = px.bar(df_plotly, y="vote_count", x="id_participant", color="phase", category_orders=dict(id_participant=participant_list),\
+    color_discrete_map={"Complete": "green", "JITAI Phase": "yellow", "Initial Phase": "blue"}, labels=plotly_labels)
 
-ax.tick_params(axis='x', labelrotation=90)
+  fig.update_layout(title_text="vote_count counts overall, individual", legend_title_text="Phases")
 
-st.pyplot(fig)
+  st.plotly_chart(fig, use_container_width=True)
 
-stop_of_plotting_bar_1 = time.time()
-print(f"Time taken to plot bar charts: {stop_of_plotting_bar_1 - start_of_plotting_bar_1} seconds")
+  # --- END OF BAR CHART ---
 
+  # --- LINE CHART ---
+  
+  df = df_storage.copy()
+  #df = df[(df.index>date_start) & df.vote_count.notna()]
+  df = df[df.vote_count.notna()]
+  df.id_participant.unique()
+  df['vote_count_count'] = (df
+   .groupby('id_participant')["vote_count"]
+   .cumcount()
+  )
+
+  df_line_chart = df.copy().reset_index().rename(columns={"index": "date_time"})
+
+  grey_bg = [{'type': 'rect',
+    'xref': 'paper',
+    'yref': 'y',
+    'x0': 0,
+    'y0': 100,
+    'x1': 1,
+    'y1': 190,
+    'fillcolor': '#b5b5b5',
+    'opacity': 0.5,
+    'layer': 'below',
+    'line_width': 0}]
+
+  fig = px.line(df_line_chart, x="date_time", y="vote_count", color="id_participant", labels=plotly_labels)
+  fig.update_layout(title_text="vote_count cumcounts, individual", legend_title_text="Experiment ID", shapes=grey_bg)
+  st.plotly_chart(fig, use_container_width=True)
+  
+  # --- END OF LINE CHART ---
+
+  st.caption("On the legend, double-click the desired item to view the chart for that item. Single-click to exclude item.")
 
 print("Hello World Done")
